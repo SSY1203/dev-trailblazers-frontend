763339c29347972d5f92d0d06904affc42946d49
SSY1203 <tlstjsdud566@naver.com>
2024-03-03 22:32:42 +0900

    [feat] 페이지네이션 쌍화살표 추가 #27

--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -27,24 +27,27 @@ const Pagination = ({
     <nav className="flex justify-center mt-6">
       <ul className="iconAndText gap-0 -space-x-px h-8 text-sm">
         <li>
-          <button disabled={noPrev} onClick={() => setCurrentPage(start - 1)}>
+          <button
+            disabled={noPrev}
+            onClick={() => setCurrentPage(start - PAGES_PER_SECTION)}
+          >
+            <span className="center pagination paginationIcon rounded-s-lg">
+              <span className="sr-only">Double Previous</span>
+              <span className="material-symbols-outlined">
+                keyboard_double_arrow_left
+              </span>
+            </span>
+          </button>
+        </li>
+
+        <li>
+          <button
+            disabled={currentPage === 1}
+            onClick={() => setCurrentPage(currentPage - 1)}
+          >
             <span className="center pagination paginationIcon rounded-s-lg">
               <span className="sr-only">Previous</span>
-              <svg
-                className="w-2.5 h-2.5 rtl:rotate-180"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 6 10"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M5 1 1 5l4 4"
-                />
-              </svg>
+              <span className="material-symbols-outlined ">chevron_left</span>
             </span>
           </button>
         </li>
@@ -67,26 +70,26 @@ const Pagination = ({
 
         <li>
           <button
+            disabled={totalPages === currentPage}
+            onClick={() => setCurrentPage(currentPage + 1)}
+          >
+            <span className="center pagination paginationIcon rounded-e-lg">
+              <span className="sr-only">Next</span>
+              <span className="material-symbols-outlined ">chevron_right</span>
+            </span>
+          </button>
+        </li>
+
+        <li>
+          <button
             disabled={noNext}
             onClick={() => setCurrentPage(start + PAGES_PER_SECTION)}
           >
             <span className="center pagination paginationIcon rounded-e-lg">
-              <span className="sr-only">Next</span>
-              <svg
-                className="w-2.5 h-2.5 rtl:rotate-180"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 6 10"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="m1 9 4-4-4-4"
-                />
-              </svg>
+              <span className="sr-only">Double Next</span>
+              <span className="material-symbols-outlined">
+                keyboard_double_arrow_right
+              </span>
             </span>
           </button>
         </li>
